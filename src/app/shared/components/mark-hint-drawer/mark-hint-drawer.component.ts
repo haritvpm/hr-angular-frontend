cd959b92fa0735870e885c4a02d512ed747b4bb4
hari <harirs@gmail.com>
2024-05-13 07:18:42 +0530

    single punch marking in prog

--- src/app/shared/components/mark-hint-drawer/mark-hint-drawer.component.ts
+++ src/app/shared/components/mark-hint-drawer/mark-hint-drawer.component.ts
@@ -87,7 +87,8 @@ export class MarkHintDrawerComponent implements OnInit {
       (logged_in_user_is_so && !this.data.punchingInfo.finalized_by_controller)
     ) {
       this.canMarkLeave = true;
-      this.canMarkSinglePunch = (/*this.data.punchingInfo.punching_count == 1 || can be singlepunch if they punch twice within minutes at evening*/
+      this.canMarkSinglePunch = !this.data.calender.is_today && !this.data.calender.future_date &&
+                (/*this.data.punchingInfo.punching_count == 1 || can be singlepunch if they punch twice within minutes at evening*/
                 !this.data.punchingInfo.single_punch_type ||
                 (this.data.punchingInfo.single_punch_type !== null &&
                 this.data.punchingInfo.single_punch_regularised_by == null));
