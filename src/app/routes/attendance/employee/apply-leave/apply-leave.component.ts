d07ed979eb0924510195c8500bb12295b9097c15
hari <hari.rs@gmail.com>
2024-05-16 15:25:37 +0530

    leave in prog

--- src/app/routes/attendance/employee/apply-leave/apply-leave.component.ts
+++ src/app/routes/attendance/employee/apply-leave/apply-leave.component.ts
@@ -8,6 +8,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { NgxMultipleDatesModule } from 'ngx-multiple-dates';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-apply-leave',
@@ -27,18 +28,19 @@ export class ApplyLeaveComponent implements OnInit, OnDestroy {
   //   new Date('2024-01-26'),
 
   // ];
-  fromDateSubscription: any = null;
+  private subscriptions : Subscription[] = [];
+  
   readonly casualTypes = [{ value: 'full', label: 'Full Day' }, { value: 'fn', label: 'Forenoon' }, { value: 'an', label: 'Afternoon' }];
-
+  sameDate : boolean = false;
   casualFromTypes = this.casualTypes;
   casualToTypes = this.casualTypes;
 
   applyLeaveForm = this.fb.group({
     leaveType: ['', Validators.required],
     fromDate: ['', Validators.required],
-    fromType: ['full'],
+    fromType: [''],
     toDate: [''],
-    toType: ['full'],
+    toType: [{ value: '', disabled: true }, ], //disable this to start with
     reason: ['', Validators.required],
     inLieofDates: [''],
 
@@ -77,42 +79,85 @@ export class ApplyLeaveComponent implements OnInit, OnDestroy {
 
     // });
 
-    this.fromDateSubscription = this.applyLeaveForm.valueChanges
+    this.subscriptions.push(this.applyLeaveForm.get('fromDate')!.valueChanges
       .subscribe((value) => {
-        console.log('form valueChanges' + JSON.stringify(value));
+        this.onFromDateChange(value);
+      }));
+    
+    this.subscriptions.push(this.applyLeaveForm.get('toDate')!.valueChanges
+      .subscribe((value) => {
+        this.onToDateChange(value);
+      }));
 
-        if (value.fromDate && value.toDate &&
-          (value.fromDate.toString() === value.toDate.toString())) {
-          console.log('fromDate and toDate are same');
-
-          if (value.leaveType === 'casual') {
-            //reset toTypes if ot was cchanged by date change .see below else block
-            this.casualFromTypes = this.casualTypes;
-            this.casualToTypes = this.casualTypes;
-            this.applyLeaveForm.get('toType')?.setValue(value.fromType!, { emitEvent: false }); //emitEvent: false to avoid infinite loop
-          }
-
-
-        } else {
-          console.log('fromDate and toDate are different');
-
-          if (value.leaveType === 'casual') {
-            this.casualToTypes = this.casualTypes;
-
-            if (value.fromDate && value.toDate) { //only if toDate is set
-              this.casualFromTypes = this.casualTypes.filter(x => x.value != 'fn'); //[{ value: 'full', label: 'Full Day' }, { value: 'an', label: 'Afternoon' }];
-              this.casualToTypes = this.casualTypes.filter(x => x.value != 'an'); //[{ value: 'full', label: 'Full Day' }, { value: 'fn', label: 'Forenoon' }];
-            } else {
-              this.casualFromTypes = this.casualTypes;
-              this.casualToTypes = this.casualTypes;
-            }  //value.leaveType === 'casual' e
-          }
-
-        }
-
-      }); //end of subscription
 
   }
+
+  onFromDateChange( fromDate: any ){
+
+    const toDate = this.applyLeaveForm.get('toDate')?.value;
+    const leaveType = this.applyLeaveForm.get('leaveType')?.value;
+    //const fromType = this.applyLeaveForm.get('fromType')?.value || null;
+    this.applyLeaveForm.get('toDate')?.setValue('', { emitEvent: false }); //emitEvent: false to avoid infinite loop
+    this.applyLeaveForm.get('toType')?.setValue('', { emitEvent: false }); //emitEvent: false to avoid infinite loop
+    this.applyLeaveForm.get('fromType')?.setValue('', { emitEvent: false }); //emitEvent: false to avoid infinite loop
+    this.casualFromTypes = this.casualTypes;
+    this.casualToTypes = this.casualTypes;
+
+    this.sameDate = false;
+    
+    if (fromDate && toDate &&
+      (fromDate.toString() === toDate.toString())) {
+      console.log('fromDate and toDate are same');
+     // this.sameDate = true;
+      // if (leaveType === 'casual') {
+      //   //reset toTypes if ot was cchanged by date change .see below else block
+      //   this.casualFromTypes = this.casualTypes;
+      //   this.casualToTypes = this.casualTypes;
+      //   this.applyLeaveForm.get('toType')?.setValue(fromType, { emitEvent: false }); //emitEvent: false to avoid infinite loop
+      // }
+
+
+    } else {
+      console.log('fromDate and toDate are different');
+      //this.sameDate = false;
+    }
+    this.casualAnFnSync(leaveType, fromDate, toDate  );
+
+  }
+
+  onToDateChange( toDate: any ){
+    const fromDate = this.applyLeaveForm.get('fromDate')?.value;
+    const fromType = this.applyLeaveForm.get('fromType')?.value || null;
+    const leaveType = this.applyLeaveForm.get('leaveType')?.value;
+    
+    if (fromDate && toDate && (fromDate.toString() === toDate.toString())) {
+
+      console.log('fromDate == toDate');
+      this.sameDate = true;
+
+      if (leaveType === 'casual') {
+        this.applyLeaveForm.get('toType')?.setValue(fromType, { emitEvent: false }); //emitEvent: false to avoid infinite loop
+      }
+      
+      } else {
+        console.log('fromDate <> toDate ');
+        this.sameDate = false;
+
+        if (leaveType === 'casual') {
+          this.applyLeaveForm.get('toType')?.setValue('', { emitEvent: false }); //emitEvent: false to avoid infinite loop
+
+        }
+      }
+
+    this.casualAnFnSync(leaveType, fromDate, toDate  );
+
+    if(this.sameDate || !(this.applyLeaveForm.get('toDate')?.value)){
+      this.applyLeaveForm.get('toType')?.disable({ emitEvent: false });
+    } else {
+      this.applyLeaveForm.get('toType')?.enable({ emitEvent: false });
+    }
+  }
+
   applyLeave() {
 
     if (this.applyLeaveForm.invalid) {
@@ -126,7 +171,8 @@ export class ApplyLeaveComponent implements OnInit, OnDestroy {
       return;
     }
 
-    if (this.applyLeaveForm.value.fromDate === this.applyLeaveForm.value.toDate) {
+    if (this.applyLeaveForm.value.fromDate?.toString() 
+        === this.applyLeaveForm.value.toDate?.toString()) {
       //same date from and to
 
       //if casual, make sure fromType and toType are same
@@ -149,13 +195,28 @@ export class ApplyLeaveComponent implements OnInit, OnDestroy {
         return;
       }
     }
+  }
 
+  casualAnFnSync(leaveType:any, fromDate:any, toDate:any)
+  {
+    //if (leaveType === 'casual') 
+    {
+      this.casualToTypes = this.casualTypes;
 
+      if (!this.sameDate && fromDate && toDate) { //only if toDate is set
+        this.casualFromTypes = this.casualTypes.filter(x => x.value != 'fn'); //[{ value: 'full', label: 'Full Day' }, { value: 'an', label: 'Afternoon' }];
+        this.casualToTypes = this.casualTypes.filter(x => x.value != 'an'); //[{ value: 'full', label: 'Full Day' }, { value: 'fn', label: 'Forenoon' }];
+      } else {
+        this.applyLeaveForm.get('toType')?.setValue('', { emitEvent: false }); //emitEvent: false to avoid infinite loop
+        this.applyLeaveForm.get('fromType')?.setValue('', { emitEvent: false }); //emitEvent: false to avoid infinite loop
 
-
+        this.casualFromTypes = this.casualTypes;
+        this.casualToTypes = this.casualTypes;
+      }  //value.leaveType === 'casual' e
+    }
   }
 
   ngOnDestroy() {
-    this.fromDateSubscription?.unsubscribe();
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
   }
 }
