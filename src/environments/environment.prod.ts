d58337eb22cf502def0baec7f0f696ce3a9540e1
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-06-30 12:34:34 +0530

    environment file change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: '',
-  useHash: false,
+  // baseUrl: 'http://172.24.18.28/attendance-mgmt-backend/',
+  baseUrl: 'http://127.0.0.1',
+  useHash: true,
 };
